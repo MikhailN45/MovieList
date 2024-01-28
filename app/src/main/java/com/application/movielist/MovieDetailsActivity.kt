4864c604b907ec8c1e2c128c02a692d96cddc5b0
MikhailN45 <53788038+MikhailN45@users.noreply.github.com>
2024-01-28 12:51:01 +0300

    add movie details screen layout

--- app/src/main/java/com/application/movielist/MovieDetailsActivity.kt
+++ app/src/main/java/com/application/movielist/MovieDetailsActivity.kt
@@ -3,7 +3,6 @@ package com.application.movielist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 
 class MovieDetailsActivity : AppCompatActivity() {
