13cbb4b9f615451f439f7ecd0b3ef9a7d9214874
MikhailN45 <53788038+MikhailN45@users.noreply.github.com>
2024-01-29 04:51:54 +0300

    add fragments, movie list screen and switch to single activity

---
+++ app/src/main/java/com/application/movielist/MovieDetailsFragment.kt
@@ -0,0 +1,23 @@
+package com.application.movielist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class MovieDetailsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movie_details, container, false)
+
+        val backButtonTextView: TextView = view.findViewById(R.id.back_button_text)
+
+        return view
+
+    }
+}
---
+++ app/src/main/java/com/application/movielist/MovieListFragment.kt
@@ -0,0 +1,22 @@
+package com.application.movielist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+
+class MovieListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movie_list, container, false)
+
+        val movieCard: ImageView = view.findViewById(R.id.avengers_film_preview)
+
+        return view
+    }
+}
