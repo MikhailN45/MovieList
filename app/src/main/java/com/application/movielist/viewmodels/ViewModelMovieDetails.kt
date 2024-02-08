f2811b7cd0255a306b68c9bd9164290104d42c8b
MikhailN45 <53788038+MikhailN45@users.noreply.github.com>
2024-02-08 22:43:30 +0300

    add viewmodel classes

---
+++ app/src/main/java/com/application/movielist/viewmodels/ViewModelMovieDetails.kt
@@ -0,0 +1,8 @@
+package com.application.movielist.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ViewModelMovieDetails(private val app: Application) : AndroidViewModel(app) {
+
+}
---
+++ app/src/main/java/com/application/movielist/viewmodels/ViewModelMovieList.kt
@@ -0,0 +1,7 @@
+package com.application.movielist.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ViewModelMovieList(private val app: Application) : AndroidViewModel(app) {
+}
