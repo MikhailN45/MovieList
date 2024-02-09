876b71447186054741400d4d32cbe8a25c9a933b
MikhailN45 <53788038+MikhailN45@users.noreply.github.com>
2024-02-09 23:33:12 +0300

    add viewmodel classes and progressbar

--- app/src/main/java/com/application/movielist/viewmodels/ViewModelMovieDetails.kt
+++ app/src/main/java/com/application/movielist/viewmodels/ViewModelMovieDetails.kt
@@ -2,7 +2,23 @@ package com.application.movielist.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.application.movielist.data.MovieData
+import com.application.movielist.data.loadMovies
+import kotlinx.coroutines.launch
 
 class ViewModelMovieDetails(private val app: Application) : AndroidViewModel(app) {
+    private var _movieLiveData: MutableLiveData<MovieData> = MutableLiveData<MovieData>()
+    val movieLiveData: LiveData<MovieData>
+        get() = _movieLiveData
 
+    fun getMovie(movieId: Int) {
+        viewModelScope.launch {
+            val movies = loadMovies(app.applicationContext)
+            val movie = movies.single { it.id == movieId }
+            _movieLiveData.postValue(movie)
+        }
+    }
 }
--- app/src/main/java/com/application/movielist/viewmodels/ViewModelMovieList.kt
+++ app/src/main/java/com/application/movielist/viewmodels/ViewModelMovieList.kt
@@ -2,6 +2,26 @@ package com.application.movielist.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.application.movielist.data.MovieData
+import com.application.movielist.data.loadMovies
+import kotlinx.coroutines.launch
 
 class ViewModelMovieList(private val app: Application) : AndroidViewModel(app) {
+    private var _movieListLideData: MutableLiveData<List<MovieData>> = MutableLiveData(emptyList())
+    val movieListLiveData: LiveData<List<MovieData>>
+        get() = _movieListLideData
+    private var _loadingLiveData: MutableLiveData<Boolean> = MutableLiveData()
+    val loadingLiveData: LiveData<Boolean>
+        get() = _loadingLiveData
+
+    fun getMovies() {
+        viewModelScope.launch {
+            _loadingLiveData.postValue(true)
+            _movieListLideData.postValue(loadMovies(app.applicationContext))
+            _loadingLiveData.postValue(false)
+        }
+    }
 }
